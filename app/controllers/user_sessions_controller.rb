327092eec3e1d4f391db5975cb9481c3f9a250ed
Cody Rayment <crayment16@gmail.com>
2009-05-06 18:09:04 -0600

    URL for login is login instead of user_session/new

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -19,7 +19,7 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     flash[:notice] = "Logout successful!"
-    redirect_back_or_default new_user_session_url
+    redirect_back_or_default root_url
   end
 end
 
