f8b4cdab08010abd7c1b58c5a1255f9d41f2c6e6
Cody Rayment <crayment16@gmail.com>
2009-05-08 12:19:48 -0600

    organizing (no changes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
+  map.register '/register/:activation_code', :controller => 'activations', :action => 'new'
+  map.activate '/activate/:id', :controller => 'activations', :action => 'create'
   
   map.resource :account, :controller => "users"
   map.resource :user_session
   
   map.resources :users
   map.resources :password_resets
-  
-  map.register '/register/:activation_code', :controller => 'activations', :action => 'new'
-  map.activate '/activate/:id', :controller => 'activations', :action => 'create'
   
   map.root :controller => "user_sessions", :action => "new"
 end
