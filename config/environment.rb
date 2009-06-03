b80b0d1df188d725c700328ce87755783914e819
Cody Rayment <crayment16@gmail.com>
2009-06-03 15:10:49 -0600

    Added Thinking Sphinx to required gems

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,11 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
+  config.gem(
+    'freelancing-god-thinking-sphinx',
+    :lib         => 'thinking_sphinx',
+    :version     => '1.1.19'
+  )
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
