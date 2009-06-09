009545d2182fec9da41a713dedd0009033056763
Cody Rayment <crayment16@gmail.com>
2009-06-09 16:35:45 -0600

    Changed existing javascript over to MooTools.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function remove_field(element, item) {
-  element.up(item).remove();
-}
+function remove_parent_element(element){
+  element.getParent().dispose();
+};
+
+
+function add_p_to_element(html, element){
+  e = new Element("p");
+  e.set('html', html);
+  e.inject($(element));
+};
