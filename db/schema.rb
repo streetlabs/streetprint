4f620dbecf61a0cba0c3234aec84013c38b62aaa
Cody Rayment <crayment16@gmail.com>
2009-07-23 16:48:56 -0600

    updating import task to support revrom

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090715184909) do
+ActiveRecord::Schema.define(:version => 20090723210248) do
 
   create_table "authoreds", :force => true do |t|
     t.integer  "author_id"
@@ -47,7 +47,8 @@ ActiveRecord::Schema.define(:version => 20090715184909) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "site_id",     :null => false
+    t.integer  "site_id",      :null => false
+    t.integer  "reference_id"
   end
 
   create_table "items", :force => true do |t|
