095c9cc304bfe0e569ab49394bb2e8e63c72c25f
Cody Rayment <crayment16@gmail.com>
2009-07-27 18:05:08 -0600

    Add RDFa support.

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "authlogic"
+  config.gem "authlogic",
+    :version => "2.1.1"
+  
   config.gem(
     'freelancing-god-thinking-sphinx',
     :lib         => 'thinking_sphinx',
@@ -29,6 +31,10 @@ Rails::Initializer.run do |config|
     :version => '2.3.11',
     :lib => 'will_paginate',
     :source => 'http://gems.github.com'
+    
+  config.gem 'rdfa',
+    :version => '0.0.8'
+
     
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
