c0ca9b27a4ff7067e38ba7a466d48567f42035aa
Mark Madsen <madsen@Ephemeral.local>
2010-01-24 02:48:48 -0700

    Removed brackets from our outgoing email to address an issue with the sender field:wq

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
   
   def password_reset_instructions(user)
     subject       "Password Reset Instructions"
-    from          "Streetprint <noreply@streetprint.org>"
+    from          "noreply@streetprint.org"
     recipients    user.email
     sent_on       Time.now
     body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
@@ -10,7 +10,7 @@ class Notifier < ActionMailer::Base
   
   def activation_instructions(user)
     subject       "Activation Instructions"
-    from          "Streetprint <noreply@streetprint.org>"
+    from          "noreply@streetprint.org"
     recipients    user.email
     sent_on       Time.now
     body          :account_activation_url => register_url(user.perishable_token)
@@ -18,7 +18,7 @@ class Notifier < ActionMailer::Base
 
   def activation_confirmation(user)
     subject       "Welcome to Streetprint"
-    from          "Streetprint <noreply@streetprint.org>"
+    from          "noreply@streetprint.org"
     recipients    user.email
     sent_on       Time.now
     body          :root_url => root_url
