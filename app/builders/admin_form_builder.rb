11b7b691bc36918c379b5eedbe91c65a92e95bfe
Mark Madsen <madsen@Ephemeral.local>
2010-02-07 22:06:53 -0700

    Cleaned up item forms and added information on form fields for users.

--- app/builders/admin_form_builder.rb
+++ app/builders/admin_form_builder.rb
@@ -2,12 +2,12 @@ class AdminFormBuilder < ActionView::Helpers::FormBuilder
   
   %w[password_field text_area text_field].each do |method_name|
     define_method(method_name) do |field_name, *args|
-      @template.content_tag(:p, field_label_with_colon(field_name, *args) + super)
+      @template.content_tag(:div, field_label_with_colon(field_name, *args) + super, :class =>'form_field')
     end
   end
   
   def check_box(field_name, *args)
-    @template.content_tag(:p,  field_label_with_colon(field_name, *args) + " " + super)
+    @template.content_tag(:div,  field_label_with_colon(field_name, *args) + " " + super, :class =>'check_box')
   end
   
   def legend(field_name, *args)
@@ -29,7 +29,7 @@ class AdminFormBuilder < ActionView::Helpers::FormBuilder
   end
   
   def collection_select_labeled(field_name, *args)
-    @template.content_tag(:p, field_label_with_colon(field_name, *args) + collection_select(field_name, *args))
+    @template.content_tag(:div, field_label_with_colon(field_name, *args) + collection_select(field_name, *args), :class =>'collection_select')
   end
   
   def text_field(field_name, *args)
@@ -38,9 +38,9 @@ class AdminFormBuilder < ActionView::Helpers::FormBuilder
     if strip == true
       super
     elsif options[:label]
-      @template.content_tag(:p, field_label_with_colon(options[:label], *args) + super)
+      @template.content_tag(:div, field_label_with_colon(options[:label], *args) + super, :class =>'text_field')
     else
-      @template.content_tag(:p, field_label_with_colon(field_name, *args) + super)
+      @template.content_tag(:div, field_label_with_colon(field_name, *args) + super, :class =>'text_field')
     end
   end
   
